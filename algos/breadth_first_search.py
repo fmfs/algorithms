7300d327bad1915c1b29ad7c14d93f483b11b8c4
fmfs <45946497+fmfs@users.noreply.github.com>
2023-05-25 02:15:49 +0200

    Update breadth_first_search.py

--- algos/breadth_first_search.py
+++ algos/breadth_first_search.py
@@ -1,6 +1,4 @@
 # Breadth First Search (BFS) Algorithm
-# Note: objective() is a method that verifies if visiting_node is the desired 
-# node.
 
 def breadth_first_search(graph, start):
   queue += deque([node])
